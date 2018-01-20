3a55c6a0569ef469385861399b38f44e72ac51dd
Martin <martin@Martin-PC.com>
2018-01-20 16:53:17 +0100

    colonie finie 'normalement'

--- Colonie.py
+++ Colonie.py
@@ -1,6 +1,6 @@
 import overpy
 import requests
-
+import math
 #COLONIE
 
 #{"_id":"5a5e71a2734d1d347185192c",
@@ -31,35 +31,56 @@ class Colonie(self,args[]):
         requests.post("https://f24h2018.herokuapp.com/auth/local",data = {"email":"ant2@sugar.ant","password":"Baby"}),
         requests.post("https://f24h2018.herokuapp.com/auth/local",data = {"email":"ant3@sugar.ant","password":"Baby"}),
         requests.post("https://f24h2018.herokuapp.com/auth/local",data = {"email":"ant4@sugar.ant","password":"Baby"})]
-        self.seedColonie=
-        self.position = {lon:x,lat:y,time:t} #Position de la Colonie
-        self.listeFourmis = [Fourmi(0,self),Fourmi(1,self),Fourmi(2,self),Fourmi(3,self)] #Liste des fourmis de la colonie
+        self.seedColonie="5a5e71a2734d1d347185192c"
+        self.position = [47.99026,0.23991] #Position de la Colonie
+        self.listeFourmis = [Fourmi(0,self,tokens[0]),Fourmi(1,self,tokens[1]),Fourmi(2,self,tokens[2]),Fourmi(3,self,tokens[3])] #Liste des fourmis de la colonie
         self.graines = 0 #Nombre de graines actuellement dans la colonie
 
 
 
     #Getter pour la position
     def getPosition(self):
-        return self.position
-
+        return "5a5e71a2734d1d347185192c"
+    #Ajoute une graine à la colonie et renvoi une fourmie
     def ajouterGraine(self,id):
         self.graines += 1
         self.listeFourmis[id].lancer()
+
+    def graineLaPlusProche(self,seedList,token):
+        i=0
+        min = 20
+        for seed in seedList:
+            if (seed["type"]="seed"):
+                seedcoord[i]={seed["location"]["coordinates"]}
+                i+=1
+        i=0
+        for seed in seedcoord:
+            if (min > math.sqrt(((seed[0]-self.position[0])**2)+((seed[1]-self.position[1])**2))):
+                min = i
+            i+=1;
+
+        requests.post("https://f24h2018.herokuapp.com/api/tracks",data = {"name":"lapin","info":"","startSeedId":self.seedColonie,"endSeedId":seedList[i]["_id"]}
+        requests.post("https://f24h2018.herokuapp.com/api/tracks",data = {"name":"lapin","info":"","startSeedId":seedList[i]["_id"],"endSeedId":self.seedColonie}
+        return(seedList[i]["location"]["coordinates"])
+
+
     #Envoi une fourmi chercher une graine.
     def lancerFourmi(self,idFourmi):
         #
         token = tokens[idFourmi]
-        u
         header = headers{'Authorization':token}
+        seedList = request.get("https://f24h2018.herokuapp.com/api/seeds/search",headers=header)
 
         r = requests.post(url,headers=header)
 
         api=overpy.Overpass()
         #Récup pos arrivée;
+        cible = graineLaPlusProche(self,seedlist,token)
 
         nom="lapin"
-        aller = Trajet(nom,"",self.position,arrive)
-        retour= Trajet("".join(reversed(nom)),"",arrive,self.position)
+
+        aller = Trajet(nom,"",self.position,cible)
+        retour= Trajet("".join(reversed(nom)),"",cible,self.position)
         self.listeFourmis[idFourmi].setAller(aller)
         self.listeFourmis[idFourmi].setRetour(retour)
         self.listeFourmis[idFourmi].lancer()
