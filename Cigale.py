0e25d9592c1f17b10366be8f1cc315bbc10d0600
Vmorgant <victor.morgant@gmail.com>
2018-01-20 18:29:04 +0100

    ajout de differents cas d'erreur

--- Cigale.py
+++ Cigale.py
@@ -1,16 +1,31 @@
 import overpy
 import requests
 import fourmi.py
+import trajet.py
 """Classe représentant la cigale qui valide ou invalide les trajets des équipes."""
 class Cigale(self, id, colonie) :
     
     def __init__(self, equipe):
         self.erreur=False
     
-    def getListeTrajet()
+    def getListeTrajet(self) :
         return(requests.get("https://f24h2018.herokuapp.com/api/tracks/otherTeams"))
-    def getPosition(trajets)
-        return(requests.get("https://f24h2018.herokuapp.com/api/tracks/:id/positions",)
     
+    def getPosition(self,trajets) : 
+        return(requests.get("https://f24h2018.herokuapp.com/api/tracks/:5a6350688fb12f001481b340/positions")
+    
+    def verifPosition(self,position) :
+        
+        #envoyer id trajet+erreur
+               
+    def verifPosition(self,position) :
+        position["lon"]
+        if (position["highway"]!="primary" && position["highway"]!="secondary" && position["highway"]!="tertiary") :
+            self.erreur=True
+        else if (position["surface"]!=asphalt) :
+            self.erreur=True
+        else if(position["access"]!="yes" || position["access"]!="unknown") :
+            self.erreur=True
+
     
     
