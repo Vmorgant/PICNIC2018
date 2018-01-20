690ca40d200e0598ef6c252bff876d8ec52d22a9
Vmorgant <victor.morgant@gmail.com>
2018-01-20 15:58:26 +0100

    Trajet

--- Trajet.py
+++ Trajet.py
@@ -12,7 +12,7 @@ class Trajet():
 
 		# fetch all ways and nodes
 		result = api.query("""
-			way("""+depart["lat"]+""","""+depart["lon"]+""","""+arrive["lat"]+""","""+arrive["lon"]+""") ["highway"];
+			way("""+depart[0]+""","""+depart[1]+""","""+arrive[0]+""","""+arrive[1]+""") ["highway"];
 			(._;>;);
 			out body;
 			""")
@@ -21,8 +21,11 @@ class Trajet():
 	#construit le tableau des routes nécessaire au trajet en rajoutant les routes a la fin du tableau
 	def construireRoutes(self, trajet):
 		for route in trajet.way:
+			print route.tags.get("oneway","n/a")
 			self.routes.append(Route(route))
 	
 	#return tableau de routes pour classe fourmis		
 	def getRoutes(self)
 		return routes
+		
+trajet = Trajet("rfqegzfruqez","info",[47.9844782,0.2415538],[47.984393,0.236012])
