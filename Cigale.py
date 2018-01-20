bef2c7c23fd3b0c4a65a6f9cdd5ece6dd2f207ef
Vmorgant <victor.morgant@gmail.com>
2018-01-20 16:06:46 +0100

    debut de la cigale

---
+++ Cigale.py
@@ -0,0 +1,16 @@
+import overpy
+import requests
+import fourmi.py
+"""Classe représentant la cigale qui valide ou invalide les trajets des équipes."""
+class Cigale(self, id, colonie) :
+    
+    def __init__(self, equipe):
+        self.erreur=False
+    
+    def getListeTrajet()
+        return(requests.get("https://f24h2018.herokuapp.com/api/tracks/otherTeams"))
+    def getPosition(trajets)
+        return(requests.get("https://f24h2018.herokuapp.com/api/tracks/:id/positions",)
+    
+    
+    
