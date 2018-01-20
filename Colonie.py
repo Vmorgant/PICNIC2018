37946addce4b5a91831f090ea0f62883fa6b9cf6
Lebourdais <lebourdais.martin@gmail.com>
2018-01-20 12:10:10 +0100

    Ajout de la colonie(vide)

---
+++ Colonie.py
@@ -0,0 +1,6 @@
+import overpy
+"""Classe représentant la colonie de départ, l'endroit d'ou partent les fourmis et l'endroit ou elles déposent les graines"""
+class Colonie(self,args[])
+
+    #
+    def requete
