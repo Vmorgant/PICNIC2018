3dd41c6df54f30247146fca0eaa83f960908d467
Marvin <marvinmoisy@gmail.com>
2018-01-20 18:34:25 +0100

    correction et amelioration trajet

--- Route.py
+++ Route.py
@@ -2,16 +2,17 @@ import overpy
 
 api=overpy.Overpass()
 
-class Route:
-	
+class Route(object):
+
 	def __init__(self, track):
 		self.name=track.tags.get("name","n/a")
 		self.oneway=track.tags.get("oneway","n/a")
 		self.sidewalk=track.tags.get("sidewalk","n/a")
-		self.surface=track.tags.get("surface","n/a")			self.cycleway=False
+		self.surface=track.tags.get("surface","n/a")
+		self.cycleway=track.tags.get("cycleway","n/a")
 		self.rightway=track.tags.get("rightway","n/a")
 		self.lit=track.tags.get("lit","n/a")
-		self.maxspeed=track.tags.get("maxspeed","n/a")
+		self.maxspeed=track.tags.get("maxspeed","50")
 		self.nodes=[]
 		for node in track.nodes:
 			self.nodes.append([node.lat,node.lon])
@@ -21,4 +22,4 @@ class Route:
 		return self.maxspeed
 
 	def getNodes(self):
-		return self.nodes	
+		return self.nodes
--- Trajet.py
+++ Trajet.py
@@ -1,6 +1,7 @@
 import overpy
+from Route import Route
 
-class Trajet():
+class Trajet(object):
 	def __init__(self, nom, info, depart, arrive):
 		self.nom = nom		#nom du trajet
 		self.info = info	#textes descriptifs
@@ -8,27 +9,40 @@ class Trajet():
 		self.arrive = arrive 	#position de l'arrivee
 		self.routes = []
 		self.construireTrajet()
-		
+
 	#construit le trajet avec la latitude et la longitude des points d'arrivee et de depart
 	def construireTrajet(self):
 		api = overpy.Overpass()
-
 		# fetch all ways and nodes
-		result = api.query("""
-			way("""+depart[0]+""","""+depart[1]+""","""+arrive[0]+""","""+arrive[1]+""") ["highway"];
-			(._;>;);
-			out body;
-			""")
-		self.construireRoutes(result)
-		
+		query = """[out:json];way("""+str(self.depart[0])+""","""+str(self.depart[1])+""","""+str(self.arrive[0])+""","""+str(self.arrive[1])+""")[highway];(._;>;);out;"""
+		lesCheminsPossibles = api.query(query)
+
+		leChemin = self.chercherMeilleurChemin(lesCheminsPossibles)
+		self.construireRoutes(leChemin)
+
+
+	def chercherMeilleurChemin(self,lesChemins):
+		lesCheminsBon = []
+		for chemin in lesChemins.ways:
+			type = chemin.tags.get("highway")
+			#print(chemin.tags.get("oneway"))
+			if( (type == "tertiary" or type == "secondary" or type == "primary") and (chemin.tags.get("oneway") != -1)):
+				lesCheminsBon.append(chemin)
+		minLong=900
+		meilleurChemin = None
+		for chemin in lesCheminsBon:
+			if(len(chemin.nodes) < minLong):
+				minLong = len(chemin.nodes)
+				meilleurChemin = chemin
+		return meilleurChemin
+
 	#construit le tableau des routes nécessaire au trajet en rajoutant les routes a la fin du tableau
 	def construireRoutes(self, trajet):
-		for route in trajet.way:
-			print(route.tags.get("oneway","n/a"))
-			self.routes.append(Route(route))
-	
-	#return tableau de routes pour classe fourmis		
+			self.routes = Route(trajet)
+
+	#return tableau de routes pour classe fourmis
 	def getRoutes(self):
-		return routes
-		
-trajet = Trajet("rfqegzfruqez","info",[47.9844782,0.2415538],[47.984393,0.236012])
+		return self.routes
+#trajet = Trajet("rfqegzfruqez","info",[47.984393,0.236012],[47.984946,0.238951])
+trajet = Trajet("rfqegzfruqez","info",[47.981339, 0.233318],[47.9844782,0.2415538])
+print(trajet.routes)
